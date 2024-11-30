a4983685aba4b68673ab8cca20776c0b6c026333
AriasJonathan30s <jacarijon@gmail.com>
2024-11-30 02:12:34 -0600

    fix commands

--- prospiSecurity.bat
+++ prospiSecurity.bat
@@ -1,8 +1,7 @@
 @echo off
 TITLE Prospi Security
 
-git diff --quiet || git stash && git pull origin master
-npm i --save
+git diff --quiet || (git stash && git pull origin master && npm i --save)
 
 start npm test
 exit
