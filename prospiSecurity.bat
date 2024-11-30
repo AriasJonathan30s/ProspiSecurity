06d33c3d9785a577f06fd3ad9b490159c19f55c2
AriasJonathan30s <jacarijon@gmail.com>
2024-11-30 03:23:02 -0600

    fix files

--- prospiSecurity.bat
+++ prospiSecurity.bat
@@ -1,10 +1,5 @@
 @echo off
 TITLE Prospi Security
 
-git diff --quiet || (
-    git stash
-    git pull
-    npm i --save )
-
 npm test
 exit
---
+++ prospiSecurityUpdr.bat
@@ -0,0 +1,5 @@
+@echo off
+TITLE Prospi Security Updater
+
+git diff --quiet || git stash git pull npm i --save
+exit
